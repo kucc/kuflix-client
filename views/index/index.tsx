965844eef6723ead9704487397099439d6b6886c
swimme <2sy100@likelion.org>
2021-04-27 02:09:44 +0900

    feat: 홈화면 레이아웃

--- views/index/index.tsx
+++ views/index/index.tsx
@@ -3,21 +3,62 @@ import { ContainerTitle } from '../../components/container-title/styles';
 import Layout from '../../components/layout';
 import { getMockdata } from './mock-data';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { NextPage } from 'next';
 
 interface IndexPageProps {
   movieData;
 }
 const IndexPage: NextPage<IndexPageProps> = ({ movieData }) => {
+  const router = useRouter();
   const [movies] = useState(movieData);
+
+  const clickHandler = (movieId: number) => {
+    router.push(`/movie/${movieId}`);
+  };
 
   return (
     <Layout>
-      <ContainerTitle>감동적인 영화</ContainerTitle>
+      <S.IndexPage>
+        <ContainerTitle>감동적인 영화</ContainerTitle>
+        <S.MovieGroup>
+          {movies.map((movie) => {
+            return (
+              <S.MovieImage
+                src={movie.image}
+                key={movie.id}
+                onClick={() => clickHandler(movie.id)}
+              />
+            );
+          })}
+        </S.MovieGroup>
 
-      <ContainerTitle>웃음을 주는 영화</ContainerTitle>
+        <ContainerTitle>웃음을 주는 영화</ContainerTitle>
+        <S.MovieGroup>
+          {movies.map((movie) => {
+            return (
+              <S.MovieImage
+                src={movie.image}
+                key={movie.id}
+                onClick={() => clickHandler(movie.id)}
+              />
+            );
+          })}
+        </S.MovieGroup>
 
-      <ContainerTitle>가족과 함께 보는 영화</ContainerTitle>
+        <ContainerTitle>가족과 함께 보는 영화</ContainerTitle>
+        <S.MovieGroup>
+          {movies.map((movie) => {
+            return (
+              <S.MovieImage
+                src={movie.image}
+                key={movie.id}
+                onClick={() => clickHandler(movie.id)}
+              />
+            );
+          })}
+        </S.MovieGroup>
+      </S.IndexPage>
     </Layout>
   );
 };
---
+++ views/index/styles.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const IndexPage = styled.div`
+  width: 100%;
+  padding-left: 20px;
+`;
+
+export const MovieGroup = styled.div`
+  width: 100%;
+  overflow: auto;
+  white-space: nowrap;
+
+  margin-bottom: 35px;
+`;
+
+export const MovieImage = styled.img`
+  width: 135px;
+  height: 189px;
+  border-radius: 12px;
+
+  margin-right: 12px;
+`;
