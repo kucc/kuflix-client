2b5a2038c73a0b625b6aeec140081189d9c5a25f
swimme <2sy100@likelion.org>
2021-04-27 03:10:51 +0900

    feat: 마이페이지 샘플데이터 추가

--- views/user/index.tsx
+++ views/user/index.tsx
@@ -1,11 +1,46 @@
+import * as S from './styles';
+import { NextPage } from 'next';
+import { useState } from 'react';
+import ContainerTitle from '../../components/container-title';
 import Layout from '../../components/layout';
+import { getMockdata } from './mock-data';
 
-const UserPage = () => {
+interface UserPageProps {
+  userData;
+  reviewData;
+  quoteData;
+}
+
+const UserPage: NextPage<UserPageProps> = ({ userData, reviewData, quoteData }: UserPageProps) => {
+  const [user] = useState(userData);
+  const [reviews] = useState(reviewData);
+  const [quotes] = useState(quoteData);
+
   return (
     <Layout>
-      <h1>마이페이지</h1>
+      <S.UserPage>
+        <S.UserContainer>
+          <ContainerTitle>내 정보</ContainerTitle>
+        </S.UserContainer>
+
+        <S.UserContainer>
+          <ContainerTitle>내가 쓴 리뷰</ContainerTitle>
+        </S.UserContainer>
+
+        <S.UserContainer>
+          <ContainerTitle>내가 쓴 명대사</ContainerTitle>
+        </S.UserContainer>
+      </S.UserPage>
     </Layout>
   );
+};
+
+UserPage.getInitialProps = async () => {
+  const userData = await getMockdata('user');
+  const reviewData = await getMockdata('review');
+  const quoteData = await getMockdata('quote');
+
+  return { userData, reviewData, quoteData };
 };
 
 export default UserPage;
---
+++ views/user/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const UserPage = styled.div`
+  width: 100%;
+  padding: 0 30px;
+`;
+
+export const UserContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 40px;
+`;
