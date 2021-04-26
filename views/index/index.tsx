2c62c9c20aaf73c318d14c7b88a3ecab8318e54e
swimme <2sy100@likelion.org>
2021-04-27 02:08:28 +0900

    feat: 홈화면 영화포스터 임시데이터 추가

--- views/index/index.tsx
+++ views/index/index.tsx
@@ -1,11 +1,31 @@
+import * as S from './styles';
+import { ContainerTitle } from '../../components/container-title/styles';
 import Layout from '../../components/layout';
+import { getMockdata } from './mock-data';
+import { useState } from 'react';
+import { NextPage } from 'next';
 
-const IndexPage = () => {
+interface IndexPageProps {
+  movieData;
+}
+const IndexPage: NextPage<IndexPageProps> = ({ movieData }) => {
+  const [movies] = useState(movieData);
+
   return (
     <Layout>
-      <h1>감동을 주는 영화</h1>
+      <ContainerTitle>감동적인 영화</ContainerTitle>
+
+      <ContainerTitle>웃음을 주는 영화</ContainerTitle>
+
+      <ContainerTitle>가족과 함께 보는 영화</ContainerTitle>
     </Layout>
   );
+};
+
+IndexPage.getInitialProps = async () => {
+  const movieData = await getMockdata();
+
+  return { movieData };
 };
 
 export default IndexPage;
