48cea24c46059203f37a2fe2fb371d44d7202408
swimme <2sy100@likelion.org>
2021-04-27 02:01:01 +0900

    fix: global background-color, padding css 수정

--- components/layout/styles.tsx
+++ components/layout/styles.tsx
@@ -7,14 +7,8 @@ export const Layout = styled.div`
 
 export const Content = styled.div`
   width: 100%;
-
-  margin-top: 50px;
-  padding: 0 30px;
-
   display: flex;
   justify-content: center;
-`;
 
-export const Container = styled.div`
-  width: 100%;
+  margin-top: 35px;
 `;
