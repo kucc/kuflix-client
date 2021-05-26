921c88895aefe6e15f2ab1f61abecda4b0113503
swimme <2sy100@likelion.org>
2021-05-26 19:55:59 +0900

    feat: react-query 패키지 적용

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
+queryClient.setDefaultOptions({
+  queries: {
+    staleTime: Infinity,
+  },
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* DEV ONLY */}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <Component {...pageProps} />{' '}
+    </QueryClientProvider>
+  );
+}
