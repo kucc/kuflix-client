c187407cca17f12314dd8468bf697811aee14138
cherry-in <sag06078@naver.com>
2021-06-30 00:58:26 +0900

    feat: quote form 작성 기능 추가

--- views/new-quote/hooks.tsx
+++ views/new-quote/hooks.tsx
@@ -1,3 +1,37 @@
-export const useQuote = () => {
-    return
-}
+import { useState } from 'react';
+import subjectAPI from '../../common/api/subject';
+
+export const useQuote = (subjectId) => {
+  const [quoteForm, setquoteForm] = useState({
+    content: '',
+  });
+
+  const handleChange = (e) => {
+    setquoteForm({ ...quoteForm, [e.target.name]: e.target.value });
+  };
+
+  const disabled = !Object.values(quoteForm).every((el) => Boolean(el));
+
+  const handleSubmit = async () => {
+    setquoteForm(quoteForm);
+    const data: { famousLineId: number } = await subjectAPI.postSubjectQuote(
+      subjectId,
+      quoteForm.content
+    );
+    if (!data) {
+      return {
+        redirect: {
+          destination: '/error',
+          permanent: false,
+        },
+      };
+    }
+  };
+  return {
+    quoteForm,
+    setquoteForm,
+    handleChange,
+    disabled,
+    handleSubmit,
+  };
+};
--- views/new-quote/index.tsx
+++ views/new-quote/index.tsx
@@ -1,22 +1,23 @@
 import Layout from '../../components/layout';
 import Register from '../new-review/component/register';
 import WriteHeader from '../new-review/component/writeheader';
-import Review from './component/review';
+import Quote from './component/quote';
 import * as S from './styles';
 import subjectAPI from '../../common/api/subject';
 import SubjectModel from '../../common/model/subject';
-// import { useQuote } from './hooks';
+import { useQuote } from './hooks';
 
 const NewQuotePage = ({ subject }) => {
-  // const { } = useQuote()
+  const { quoteForm, setquoteForm, handleChange, disabled, handleSubmit } = useQuote(subject.id);
   return (
     <Layout>
       <S.QuotePageContainer>
+        {console.log(quoteForm)}
         <WriteHeader name={subject.name} releasedDate={subject.releasedDate} movieId={subject.id} />
         <S.QuoteComponentContainer>
           <S.QuoteComponentTitle>명대사 작성</S.QuoteComponentTitle>
           <S.QuoteWriteContainer>
-            <Review />
+            <Quote quoteForms={quoteForm} handleChange={handleChange} />
           </S.QuoteWriteContainer>
         </S.QuoteComponentContainer>
         <S.QuoteComponentContainer>
@@ -24,7 +25,8 @@ const NewQuotePage = ({ subject }) => {
             message="명대사 등록하기"
             movieId={subject.id}
             link="complete-quote"
-            handleClick={() => {}}
+            handleClick={handleSubmit}
+            disabled={disabled}
           />
         </S.QuoteComponentContainer>
       </S.QuotePageContainer>
